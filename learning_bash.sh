07296d4165fc0d5d88a35ff9243705368967620a
surfingontheinterwebs <surfingontheinterwebs@gmail.com>
2025-01-28 17:48:59 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+greeting=hello
+name=Wolfgang
+echo "$greeting, $name"
+echo this script is helping you learn bash
+mkdir /home/wolfgang/Desktop/bioinformatics/
+mkdir /home/wolfgang/Desktop/bioinformatics/bash_scripts
+mv /home/wolfgang/Desktop/bash_template.txt /home/wolfgang/Desktop/bioinformatics/bash_scripts
+mv /home/wolfgang/Desktop/bioinformatics/bash_scripts/bash_template.txt /home/wolfgang/Desktop/bioinformatics/bash_scripts/bash_template.sh
